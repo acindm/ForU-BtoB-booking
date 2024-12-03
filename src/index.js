2f427c1b617705d01bca1d2e72bbfae474490dde
acindms <acindms@gmail.com>
2024-12-03 22:44:29 +0800

    DetailPage Update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { ThemeProvider } from 'styled-components'
 
 import App from '@/App'
 import "normalize.css"
-// import "antd/dist/antd.less"
+import "antd/dist/reset.css"
 import "./assets/css/index.less"
 import store from './store'
 import theme from './assets/theme'
