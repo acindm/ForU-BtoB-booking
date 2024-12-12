2986d20bd7e5732f1c5d08c25130baaaf8bff034
acindms <acindms@gmail.com>
2024-12-12 23:52:36 +0800

    Update

--- src/components/app-header/c-cpns/header-center/index.jsx
+++ src/components/app-header/c-cpns/header-center/index.jsx
@@ -1,11 +1,30 @@
 import IconSearchBar from '@/assets/svg/icon-search-bar'
-import React, { memo } from 'react'
+import React, { memo, useState } from 'react'
 import { CenterWrapper } from './style'
+import { CSSTransition } from "react-transition-group"
+import SearchTitles from "@/assets/data/search_titles"
+import SearchTabs from './c-cpns/search-tabs'
+import SearchSections from './c-cpns/search-sections'
 
-const HeaderCenter = memo(() => {
+const HeaderCenter = memo((props) => {
+  const { isSearch, searchBarClick } = props
+
+  const [tabIndex, setTabIndex] = useState(0)
+  const titles = SearchTitles.map(item => item.title)
+
+  function searchBarClickHandle() {
+    if (searchBarClick) searchBarClick()
+  }
+
   return (
     <CenterWrapper>
-      <div className='search-bar'>
+      <CSSTransition
+        in={isSearch}
+        timeout={250}
+        classNames='bar'
+        unmountOnExit={true}
+      >
+      <div className='search-bar' onClick={searchBarClickHandle}>
         <div className='text'>
           Search...
         </div>
@@ -13,6 +32,20 @@ const HeaderCenter = memo(() => {
           <IconSearchBar/>
         </div>
       </div>
+      </CSSTransition>
+      <CSSTransition
+        in={!isSearch}
+        timeout={250}
+        classNames='detail'
+        unmountOnExit={true}
+      >
+        <div className='search-detail'>
+          <SearchTabs titles={titles} tabClick={setTabIndex}/>
+          <div className='infos'>
+            <SearchSections searchInfos={SearchTitles[tabIndex].searchInfos}/>
+          </div>
+        </div>
+      </CSSTransition>
     </CenterWrapper>
   )
 })
--- src/components/app-header/index.jsx
+++ src/components/app-header/index.jsx
@@ -1,16 +1,45 @@
-import React, { memo } from 'react'
-import { HeaderWrapper } from './style'
+import React, { memo, useRef, useState } from 'react'
+import classNames from 'classnames'
+import { HeaderWrapper, SearchAreaWrapper } from './style'
 import HeaderLeft from './c-cpns/header-left'
 import HeaderCenter from './c-cpns/header-center'
 import HeaderRight from './c-cpns/header-right'
+import { shallowEqual, useSelector } from 'react-redux'
+import { ThemeProvider } from 'styled-components'
+import useScrollPosition from '@/hooks/useScrollPosition'
+
 
 const AppHeader = memo(() => {
+    const [isSearch, setIsSearch] = useState(false)
+
+    const { headerConfig } = useSelector((state) => ({
+      headerConfig: state.main.headerConfig
+    }), shallowEqual)
+    const { isFixed, topAlpha } = headerConfig
+  
+    const { scrollY } = useScrollPosition()
+    const prevY = useRef(0)
+    if (!isSearch) prevY.current = scrollY
+    if (isSearch && Math.abs(scrollY - prevY.current) > 30) setIsSearch(false)
+  
+    const isAlpha = topAlpha && scrollY === 0
+  
+
   return (
-    <HeaderWrapper>
-      <HeaderLeft />
-      <HeaderCenter />
-      <HeaderRight />
-    </HeaderWrapper>
+    <ThemeProvider theme={{isAlpha}}>
+      <HeaderWrapper className={classNames({ fixed: isFixed })}>
+        <div className='content'>
+          <div className='top'>
+            <HeaderLeft />
+            <HeaderCenter isSearch={ isAlpha || isSearch} searchBarClick={e => setIsSearch(true)}/>
+            <HeaderRight />
+          </div>
+          <SearchAreaWrapper isSearch={ isAlpha || isSearch}/>
+        </div>
+        { isSearch && <div className='cover' onClick={e => setIsSearch(false)}></div> }
+      </HeaderWrapper>
+    </ThemeProvider>
+
   )
 })
 
